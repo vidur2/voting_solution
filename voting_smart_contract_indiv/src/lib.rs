d48e5cd720e6e370b77f7eab5967a5f6b8d8f070
vidur2 <vmod2005@gmail.com>
2021-10-21 13:56:33 -0400

    shit

---
+++ voting_smart_contract_indiv/src/lib.rs
@@ -0,0 +1,7 @@
+use near_sdk::{near_bindgen, env, ext_contract, Gas, Balance, AccountId, Promise};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use std::f32::consts::E;
+
+fn main() {
+    println!("Hello, world!");
+}
