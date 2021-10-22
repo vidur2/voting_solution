0aad96d601a476d5d419d655026ceca145ad1083
vidur2 <vmod2005@gmail.com>
2021-10-22 13:15:49 -0400

    First Hackathon Commi

--- voting_smart_contract_collective/src/lib.rs
+++ voting_smart_contract_collective/src/lib.rs
@@ -2,6 +2,42 @@ use near_sdk::{near_bindgen, env, ext_contract, Gas, Balance, AccountId, Promise
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use std::f32::consts::E;
 
-fn main() {
-    println!("Hello, world!");
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Default)]
+pub struct Election {
+    candidate1: String,
+    candidate1_count: u128,
+    candidate2: String,
+    candidate2_count: u128,
+    candidate3: String,
+    candidate3_count: u128
+}
+
+#[near_bindgen]
+impl Election {
+    #[init]
+    pub fn new(candidate1: String, candidate2: String, candidate3: String) -> Self{
+        Self {
+            candidate1: candidate1,
+            candidate1_count: 0u128,
+            candidate2: candidate2,
+            candidate2_count: 0u128,
+            candidate3: candidate3,
+            candidate3_count: 0u128,
+        }
+    }
+
+    pub fn vote(&mut self, candidate: String, has_voted: bool){
+        let voter_id: String = env::predecessor_account_id();
+        if candidate == self.candidate1 && has_voted == false{self.candidate1_count = self.candidate1_count + 1u128}
+        else if candidate == self.candidate2 && has_voted == false{self.candidate2_count = self.candidate2_count + 1u128}
+        else if candidate == self.candidate3 && has_voted == false{self.candidate3_count = self.candidate3_count + 1u128};
+    }
+
+    pub fn get_count(&self, candidate: String) -> u128{
+        if candidate == self.candidate1{return self.candidate1_count}
+        else if candidate == self.candidate2{return self.candidate2_count}
+        else if candidate == self.candidate3{return self.candidate3_count}
+        else{return 0u128}
+    }
 }
--- voting_smart_contract_indiv/src/lib.rs
+++ voting_smart_contract_indiv/src/lib.rs
@@ -1,7 +1,41 @@
 use near_sdk::{near_bindgen, env, ext_contract, Gas, Balance, AccountId, Promise};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use std::f32::consts::E;
 
-fn main() {
-    println!("Hello, world!");
+const BASE_GAS: u64 = 5_000_000_000_000;
+const NO_DEPOSIT: Balance = 0;
+const ELECTION_ID: &str = "election.testnet";
+
+#[ext_contract(election_smart_contract)]
+pub trait Election{
+    fn vote(&mut self, candidate: String, has_voted: bool);
+}
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Default)]
+pub struct IndivididualVote {
+    has_voted: bool
+}
+
+#[near_bindgen]
+impl IndivididualVote{
+    #[init]
+    pub fn new() -> Self{
+        Self {
+            has_voted: false
+        }
+    }
+
+    #[private]
+    pub fn vote_for_person(&mut self, person: String){
+        if !self.has_voted{
+            let gas_count = Gas::from(BASE_GAS);
+            let election_account_id: AccountId = ELECTION_ID.trim().parse().expect("invalid constant");
+            election_smart_contract::vote(person, self.has_voted, election_account_id, NO_DEPOSIT, gas_count);
+            self.has_voted = true;
+        }
+    }
+
+    pub fn has_voted(&self) -> bool{
+        self.has_voted
+    }
 }
